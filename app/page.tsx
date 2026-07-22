ac12e9a7ff191a236b644e414f4bd9d781bff2ee
JuanCruzReynoso <juanosk898@gmail.com>
2026-07-22 12:33:20 -0300

    fix: force-dynamic on home page to fetch fresh featured products

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getFeaturedProducts } from "@/features/product/lib/queries";
 import { HomeContent } from "@/features/home/ui/home-content";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const siteUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
   const featured = await getFeaturedProducts();
